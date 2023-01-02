413268ce8764fbb2b8e38bad197bdb0fadc27cc3
MinhNhat-Aug27 <81581691+MinhNhat-Aug27@users.noreply.github.com>
2023-01-02 23:43:33 +0700

    fix base/views.py & login.html

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-
+from users.models import User, Customer
 
 # Register your models here.
+
+admin.register(User)
+admin.register(Customer)
--- base/views.py
+++ base/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def login(request):
     # page = 'login'
+    username = ''
+    password = ''
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
-        if username == 'group25' & password == 'group25':
-            print('superuser')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            return HttpResponse('No accounts found')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            return HttpResponse('Login successfully')
         else:
-            print('Welcome stranger')
+            return HttpResponse('Wait a minute, who are you')
 
     context = {}
-    return render(request, 'base/login.html')
+    return render(request, 'base/login.html', context)
