b008aebe649ff6d8d1b252f0c748f2297ad629a1
hưng <hungls1234506@gmail.com>
2023-01-03 15:51:48 +0700

    testgit

--- flights/views.py
+++ flights/views.py
@@ -33,3 +33,26 @@ def display(request):
     i = Customer.objects.all()
     context = {"info":i}
     return render(request,"display.html",context)
+
+airports = ["HaNoi","HCM","DaNang","DaLat" ]
+# Create your views here.
+def index(request):
+    
+    #airports = Airport.objects.all()
+    context = {'airports':airports}
+    
+    if request.method == 'GET':
+        ticket_type = request.GET.get('ticket_type')
+        class_fl = request.GET.get('class')
+        seat_number = request.GET.get('seat_number')
+        from_ap = request.GET.get('from')
+        to_ap = request.GET.get('to')
+        departure_date = request.GET.get('departure_date')
+        # retrive ticket
+        if ticket_type == 'one way':
+            pass
+        # else ticket_type = 'round trip'
+        else:
+            pass
+            
+    return render(request,'index.html',context)
