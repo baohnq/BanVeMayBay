1958fa8c5d72a54438dcdfaaea1165754edbd09d
Tan <duytan12378@gmail.com>
2023-01-06 01:24:03 +0700

    update schedule

--- flights/urls.py
+++ flights/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('create_customer/',  views.createCustomer, name="create_customer"),
+    path('create_customer/<str:scheduleID>/',  views.createCustomer, name="create_customer"),
     path('display/',  views.display, name="display"),
+    path('schedule/<str:pk>/',  views.schedule, name="schedule"),
 
 ]
--- flights/views.py
+++ flights/views.py
@@ -6,14 +6,13 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Customer
+from .models import Customer, Schedule
 from .forms import CustomerForm 
 
 # Create your views here.
 
-def createCustomer(request):
+def createCustomer(request,scheduleID):
     form = CustomerForm()
-
     if request.method == 'POST':
 
         Customer.objects.create(
@@ -22,10 +21,9 @@ def createCustomer(request):
             sdt = request.POST.get('sdt'),
         )
 
-        
         return redirect('display')
 
-    context = {'form': form}
+    context = {'form': form, "scheduleID":scheduleID}
     return render(request, 'customer_form.html', context)
 
 
@@ -33,5 +31,12 @@ def display(request):
     i = Customer.objects.all()
     context = {"info":i}
     return render(request,"display.html",context)
+
+def schedule(request,pk):
+    schedule = Schedule.objects.get(id=pk)
+    context = {"schedule":schedule,"pk":pk}
+    return render(request,"schedule.html",context)
+
+
 
     
