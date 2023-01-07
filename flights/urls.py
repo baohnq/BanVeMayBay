63c68840d803a724ebc00c3e85b1cc079e60eb37
baohnq <baohnq@unicloud.com>
2023-01-07 16:30:56 +0700

    link login main book search screen

--- flights/urls.py
+++ flights/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns =[
     path('', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
-    path('home/',views.index,name='index'),
+    path('home/',views.index,name='main'),
+    path('book/',views.index,name='index'),
     path('create_customer/',  views.createCustomer, name="create_customer"),
     path('create_user/',  views.createUser, name="create_user"),
     path('display/',  views.display, name="display"),
